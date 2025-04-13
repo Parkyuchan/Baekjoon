b54e0e2e10a43b295f5f69e8499599c8fc990413
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2025-04-13 12:00:49 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 180 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        System.out.println((long) n * n * n); // 정확한 수행 횟수
+        System.out.println(3);
+    }
+
+}
