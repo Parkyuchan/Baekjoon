699ad833638009097954ec3daec5f26e3fd4111a
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2025-08-21 16:53:55 +0900

    [Silver V] Title: 최소공배수, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long gcd = gcd(a, b);
+        long lcm = a * b / gcd;
+
+        sb.append(lcm).append("\n");
+
+        System.out.println(sb);
+    }
+
+    private static long gcd(long a, long b) {
+        while (b != 0) {
+            long tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+        return a;
+    }
+}
