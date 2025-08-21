aa95dd938520407909084b88fac86b4ce44b9105
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2025-08-21 22:07:47 +0900

    [Silver V] Title: 창문 닫기, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int count = 1;
+
+        for (int i = 2; i*i <= N; i++) {
+            count++;
+        }
+
+        System.out.println(count);
+    }
+
+}
