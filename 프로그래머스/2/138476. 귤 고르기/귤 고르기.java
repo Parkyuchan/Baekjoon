861f90bb89bc8ff851c6629c3820df55aac595c5
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2025-09-18 03:25:08 +0900

    [level 2] Title: 귤 고르기, Time: 59.45 ms, Memory: 113 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i<tangerine.length; i++) {
+            map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
+        }
+
+        List<Integer> counts = new ArrayList<>(map.values());
+        Collections.sort(counts, Collections.reverseOrder());
+
+        for (int i = 0; i<counts.size(); i++) {
+            k -= counts.get(i);
+            answer++;
+
+            if (k <= 0)
+                break;
+        }
+        return answer;
+    }
+}
