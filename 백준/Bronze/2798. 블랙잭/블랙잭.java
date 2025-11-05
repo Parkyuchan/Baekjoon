3d50ae7e43deac3357f8d33e97804af87318cb7e
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2025-11-06 03:02:51 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,33 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class Main {
+class Main {
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int num = scanner.nextInt();
-        int[] list = new int[N];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i<N; i++)
-            list[i] = scanner.nextInt();
+        int[] card = new int[N];
 
-        Arrays.sort(list);
+        st = new StringTokenizer(br.readLine());
 
+        for (int i = 0; i < N; i++)
+            card[i] = Integer.parseInt(st.nextToken());
 
-        int max = 0;
+        Arrays.sort(card);
+        int result = 0;
 
-        for(int i = 0; i<N - 2; i++) {
-            for(int j = i + 1; j<N - 1; j++) {
-                for(int k = j + 1; k<N; k++) {
-                    int sum = list[i] + list[j] + list[k];
-                    if(sum <= num && sum > max)
-                        max = sum;
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++)
+                for (int k = j + 1; k < N; k++) {
+                    int sum = card[i] + card[j] + card[k];
+                    if (sum <= M) {
+                        if (result < sum) {
+                            result = sum;
+                        }
+                    } else {
+                        break;
+                    }
                 }
-            }
         }
+        System.out.println(result);
 
-        System.out.println(max);
     }
+
 }
