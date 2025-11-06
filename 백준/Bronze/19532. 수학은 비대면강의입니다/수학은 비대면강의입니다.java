2d65d0b199cbc65db131a42433b6eff551be4ec2
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2025-11-07 01:19:26 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 156 ms, Memory: 17448 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,25 +1,26 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+class Main {
+    public static void main(String[] args) throws Exception {
 
-public class Main {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
 
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int result1 = scanner.nextInt();
-        int c = scanner.nextInt();
-        int d = scanner.nextInt();
-        int result2 = scanner.nextInt();
-
-        for(int x = -999; x<=999; x++) {
-            for(int y = -999; y<=999; y++) {
-                if(a*x + b*y == result1 && c*x + d*y == result2) {
+        for (int x = -999; x <= 999; x++) {
+            for (int y = -999; y <= 999; y++) {
+                if (a * x + b * y == c && d * x + e * y == f) {
                     System.out.println(x + " " + y);
                     return;
                 }
-
             }
         }
 
