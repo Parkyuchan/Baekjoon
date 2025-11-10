5563b70310fca16df4a8d04ac7e8e5656450255d
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2025-11-11 00:26:37 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,25 +1,25 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.io.*;
 class Main {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
 
         int remain = N % 5;
         int count = N / 5;
 
-        while(remain <= N) {
+        while (remain > 0) {
+            if (count == -1)
+                break;
+
             if (remain % 3 == 0) {
                 count += remain / 3;
                 break;
             }
 
-            remain += 5;
             count--;
+            remain += 5;
         }
 
         System.out.println(count);
