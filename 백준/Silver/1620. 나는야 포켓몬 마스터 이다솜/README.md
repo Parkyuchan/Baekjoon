c7d58049512bf8d4781c681f52283766024770ee
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2025-11-11 22:41:45 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 488 ms, Memory: 61308 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/README.md
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 57956 KB, 시간: 524 ms
+메모리: 61308 KB, 시간: 488 ms
 
 ### 분류
 
@@ -12,7 +12,7 @@
 
 ### 제출 일자
 
-2025년 7월 1일 01:45:40
+2025년 11월 11일 22:41:38
 
 ### 문제 설명
 
@@ -112,9 +112,9 @@
 
 <p><img alt="" src="https://www.acmicpc.net/upload/201004/pg.PNG" style="height:181px; width:274px"></p>
 
-<p><img alt="" src="" style="height:184px; width:274px"></p>
+<p><img alt="" src="https://www.acmicpc.net/upload/201004/ph.PNG" style="height:184px; width:274px"></p>
 
-<p><img alt="" src="" style="height:184px; width:274px"></p>
+<p><img alt="" src="https://www.acmicpc.net/upload/201004/pi.PNG" style="height:184px; width:274px"></p>
 
 <p>이다솜 : 휴... 이겼다.</p>
 
--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,37 +1,37 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+       StringTokenizer st = new StringTokenizer(br.readLine());
+       int N = Integer.parseInt(st.nextToken());
+       int M = Integer.parseInt(st.nextToken());
+       Map<String, String> name = new HashMap<>();
+       Map<String, String> index = new HashMap<>();
 
-        Map<String, Integer> map1 = new HashMap<>(N);
-        Map<Integer, String> map2 = new HashMap<>(N);
+       for (int i = 1; i <= N; i++) {
+           String input = br.readLine();
+           name.put(input, String.valueOf(i));
+           index.put(String.valueOf(i), input);
+       }
 
-        for(int i = 1; i<=N; i++) {
-            String str = br.readLine();
-            map1.put(str, i);
-            map2.put(i, str);
-        }
-        
-        for(int i = 0; i<M; i++) {
-            String question = br.readLine();
+       for (int i = 0; i < M; i++) {
+           String input = br.readLine();
+           if (name.containsKey(input))
+               sb.append(name.get(input));
+           else
+               sb.append(index.get(input));
 
-            if(map1.containsKey(question))
-                sb.append(map1.get(question)).append("\n");
-            else {
-                sb.append(map2.get(Integer.parseInt(question))).append("\n");
-            }
-        }
+           sb.append("\n");
+       }
 
         System.out.println(sb);
 
     }
+
 }
