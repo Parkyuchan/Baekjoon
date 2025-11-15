3ecba876cfb18df5b2f38dfbff6d53e6b2aa15c7
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2025-11-15 11:55:13 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 152 ms, Memory: 15992 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -1,48 +1,46 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        Deque<int[]> deque = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        
+
+        Deque<int[]> deque = new ArrayDeque<>();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        for(int i = 0; i<N; i++) {
-        	int[] tmp = new int[2];
-        	tmp[0] = i+1;
-        	tmp[1] = Integer.parseInt(st.nextToken());
-        	deque.offerLast(tmp);
+        for (int i = 0; i < N; i++) {
+            int[] tmp = new int[2];
+            tmp[0] = i + 1;
+            tmp[1] = Integer.parseInt(st.nextToken());
+            deque.add(tmp);
         }
-        
-        br.close();
-        
-        for(int i = 0; i<N; i++) {
-        	int tmp = deque.peekFirst()[1];
-        	arr[i] = deque.pollFirst()[0];
-        	if(deque.size() == 0)
-        		break;
-        	
-        	if(tmp < 0) {
-        		for(int j = 0; j<-tmp; j++)
-        			deque.offerFirst(deque.pollLast());
-        	}
-        	else
-        		for(int j = 0; j<tmp-1; j++)
-        			deque.offerLast(deque.pollFirst());
+
+        for (int i = 0; i < N; i++) {
+            int tmp = deque.peekFirst()[1];
+            sb.append(deque.pollFirst()[0]).append(" ");
+
+            if (deque.size() == 0)
+                break;
+
+            if (tmp > 0) {
+                for (int j = 0; j < tmp-1; j++) {
+                    deque.offerLast(deque.pollFirst());
+                }
+            } else {
+                for (int j = tmp; j < 0; j++) {
+                    deque.offerFirst(deque.pollLast());
+                }
+            }
+
         }
-        
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        for(int i = 0; i<N; i++)
-        	bw.write(String.valueOf(arr[i]) + " ");
-        
-        bw.flush();
-        bw.close();
+
+        System.out.println(sb);
+
+
     }
-    
-    
+
 }
