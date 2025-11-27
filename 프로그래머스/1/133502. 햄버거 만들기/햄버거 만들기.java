28a1e2faf96dac13cc2baa7e93dfef63aafe444e
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2025-11-27 17:15:38 +0900

    [level 1] Title: 햄버거 만들기, Time: 57.90 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -10,30 +10,24 @@ class Solution {
         // 1이 나온다면, 이후 4번째까지 1, 2, 3, 1형식이 나오면 됨
         
         Stack<Integer> stack = new Stack<>();
-        List<Integer> list = new ArrayList<>();
-        int len = ingredient.length;
-
-        for (int i = 0; i < len; i++)
-            list.add(ingredient[i]);
         
-        len = list.size();
-        for (int i = 0; i < len; i++) {
-            if (list.get(i) == 1 && i + 3 < len) {
-                if (list.get(i + 1) == 2
-                   && list.get(i + 2) == 3
-                   && list.get(i + 3) == 1) {
+        for (int num : ingredient)  {
+            stack.push(num);  
+        
+            if (stack.size() >= 4) {
+                int size = stack.size();
+                if (stack.get(size - 4) == 1 &&
+                   stack.get(size - 3) == 2 &&
+                   stack.get(size - 2) == 3 &&
+                   stack.get(size - 1) == 1) {
                     answer++;
+                
                     for (int j = 0; j < 4; j++)
-                        list.remove(i);
-                    i = stack.isEmpty() ? i - 1 : stack.pop() - 1;
-                    len = list.size();
-                } else {
-                    stack.push(i);
-
+                        stack.pop();
                 }
             }
-            
         }
+        
         return answer;
     }
 }
