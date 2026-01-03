6b8888d78fc6cd507c212d5ece1c7a14df43b880
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2026-01-04 00:20:42 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 3.29 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -2,26 +2,37 @@ import java.io.*;
 
 class Solution {
     public String solution(String s) {
-
-        StringBuilder sb = new StringBuilder();
-        boolean newWord = true;
-
-        for (int i = 0; i<s.length(); i++) {
-            char c = s.charAt(i);
-
-            if (c == ' ') {
-                sb.append(c);
-                newWord = true;
-            } else {
-                if (newWord) {
-                    sb.append(Character.isLetter(c) ? Character.toUpperCase(c) : c);
-                    newWord = false;
-                } else {
-                    sb.append(Character.toLowerCase(c));
-                }
-            }
+        String answer = "";
+        String[] split = s.split("\\s+");
+        int[] space = new int[split.length];
+        boolean check = false;
+        int start = -1;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (!check && s.charAt(i) == ' ') {
+                check = true;
+                space[++start]++;
+            } else if (s.charAt(i) == ' ')
+                space[start]++;
+            else if (check && s.charAt(i) != ' ')
+                check = false;
         }
-
-        return sb.toString();
+        
+        for (int i = 0; i < split.length; i++) {
+            char first = split[i].charAt(0);
+            String remain = "";
+            if (split[i].length() > 1)
+                remain = split[i].substring(1).toLowerCase();
+            
+            if (first >= 'a' && first <= 'z')
+                split[i] = String.valueOf(Character.toUpperCase(first)) + remain;
+            else split[i] = String.valueOf(first) + remain;
+            
+            answer += split[i];
+            for (int j = 0; j < space[i]; j++)
+                answer += " ";
+        }
+        
+        return answer;
     }
 }
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
 
 ### 성능 요약
 
-메모리: 84.8 MB, 시간: 0.20 ms
+메모리: 81.5 MB, 시간: 3.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 11일 16:01:49
+2026년 01월 04일 00:20:41
 
 ### 문제 설명
 
