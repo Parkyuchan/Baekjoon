ae06db33a63a60bd85cb589713ca2d7dc7341d4a
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2026-01-06 21:52:14 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 31.29 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,28 +1,42 @@
 class Solution {
     public int[] solution(String s) {
         int[] answer = {0, 0};
-
-        while(true) {
-            String re_s = s.replace("0", "");
-            answer[1] += s.length() - re_s.length();
-            answer[0]++;
-            int len = re_s.length();
-            s = "";
-            while(true) {
-                if (len % 2 == 0)
-                    s = "0" + s;
-                else
-                    s = "1" + s;
-
-                if (len == 1)
-                    break;
-                len /= 2;
-            }
-
-            if (s.equals("1"))
-                break;
+        
+        // 1. s의 0을 제거
+        // 2. 제거한 문자열의 길이 판단
+        // 3. 길이에 대한 이진수 값 최신화
+        // 반복
+        
+        int retry = 0;
+        int zero = 0;
+        
+        while (s.length() > 1) {
+            int len = s.length();
+            s = s.replaceAll("0", "");
+            int changeLen = s.length();
+            zero += len - changeLen;
+            s = toBinary(changeLen);
+            retry++;
         }
         
+        answer[0] = retry;
+        answer[1] = zero;
+        
         return answer;
+    }
+    
+    private String toBinary(int len) {
+        String re = "";
+        while(len > 0) {
+            if (len % 2 == 0) {
+                re = "0" + re;
+            } else {
+                re = "1" + re;
+            }
+            
+            len /= 2;
+        }
+        
+        return re;
     }
 }
