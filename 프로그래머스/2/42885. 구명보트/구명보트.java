f9b0d309eaab2c499cc53be8313a2f1366d074ba
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2026-01-08 18:50:50 +0900

    [level 2] Title: 구명보트, Time: 10.78 ms, Memory: 57.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -4,20 +4,15 @@ class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-        int start = 0;
-
-        for (int i = people.length - 1; i >= 0 ; i--) {
-
-            if (start > i)
-                break;
-
-            if (people[i] + people[start] <= limit) {
-                start++;
-            }
-
+        int left = 0;
+        int right = people.length - 1;
+        
+        while(left <= right) {
+            if (people[left] + people[right--] <= limit) left++;
+            
             answer++;
         }
-
+        
         return answer;
     }
 }
