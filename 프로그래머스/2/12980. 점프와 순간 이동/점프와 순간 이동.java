013a99437c4ba6f14bdcef77ee17afa02cce9f0d
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2026-01-10 20:11:04 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,13 +2,16 @@
 public class Solution {
     public int solution(int n) {
         int ans = 0;
-
-        while(n>0) {
-            if (n%2 != 0) {
-                n -= 1;
-                ans++;
-            } else
+        // K칸을 "점프"할 때만 건전지 감소
+        // 순간이동은 감소 x
+        
+        while(n > 0) {
+            if (n % 2 == 0)
                 n /= 2;
+            else {
+                n--;
+                ans++;
+            }
         }
         
         return ans;
