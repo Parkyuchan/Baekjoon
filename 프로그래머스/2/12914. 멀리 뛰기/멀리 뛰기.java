e66c7a8f059290f4fd2ec5c68f1062d11a0cd7c1
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2026-01-11 22:59:35 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.55 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -5,22 +5,25 @@ class Solution {
     public long solution(int n) {
         long answer = 0;
         dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-
+        
+        for (int i = 0; i < dp.length; i++)
+            dp[i] = -1;
+        
         answer = dfs(0, n);
 
         return answer;
     }
 
     private static int dfs(int sum, int n) {
+        
         if (sum == n)
             return 1;
-        if (sum > n)
+        else if (sum > n)
             return 0;
-
+        
         if (dp[sum] != -1)
             return dp[sum];
-
+        
         dp[sum] = (dfs(sum + 1, n) + dfs(sum + 2, n)) % 1234567;
         return dp[sum];
 
