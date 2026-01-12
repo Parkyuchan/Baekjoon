61be03ae5d661ab09a793e3ab6b423138c95bc88
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2026-01-12 23:29:28 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.32 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,57 +2,25 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = new int[2];
-        int firstIdx = 0;
-        int secondIdx = 0;
-        int[] firstAnswer = new int[2];
-        int[] secondAnswer = new int[2];
-
-        List<String> list = new ArrayList<>();
-
-        for (int i = 0; i<words.length; i++) {
-            if (list.contains(words[i]) || words[i].length() < 2 || words[i].length() > 50) {
-                int idx = (i + 1) % n;
-                firstAnswer[0] = idx == 0 ? n : idx;
-                firstAnswer[1] = idx == 0 ? (i + 1) / n : (i + 1) / n + 1;
-                firstIdx = i;
+        int[] answer = {0, 0};
+        Stack<String> stack = new Stack<>();
+        Set<String> set = new HashSet<>();
+        
+        set.add(words[0]);
+        stack.push(words[0]);
+        
+        for (int i = 1; i < words.length; i++) {
+            String pop = stack.pop();
+            if (pop.charAt(pop.length() - 1) != words[i].charAt(0) || set.contains(words[i])) {
+                answer[0] = i % n + 1;
+                answer[1] = i / n + 1;
                 break;
             }
-
-            list.add(words[i]);
+            
+            set.add(words[i]);
+            stack.push(words[i]);
         }
-
-        for (int i = 1; i<words.length; i++) {
-            char beforeWord = words[i - 1].charAt(words[i - 1].length() - 1);
-            char afterWord = words[i].charAt(0);
-
-            if (beforeWord != afterWord) {
-                int idx = (i + 1) % n;
-                secondAnswer[0] = idx == 0 ? n : idx;
-                secondAnswer[1] = (i + 1) % n == 0 ? (i + 1) / n : (i + 1) / n + 1;
-                secondIdx = i;
-                break;
-            }
-        }
-
-        if (firstIdx > secondIdx) {
-            if (secondIdx == 0) {
-                answer[0] = firstAnswer[0];
-                answer[1] = firstAnswer[1];
-            } else {
-                answer[0] = secondAnswer[0];
-                answer[1] = secondAnswer[1];
-            }
-        } else {
-            if (firstIdx == 0) {
-                answer[0] = secondAnswer[0];
-                answer[1] = secondAnswer[1];
-            } else {
-                answer[0] = firstAnswer[0];
-                answer[1] = firstAnswer[1];
-            }
-        }
-
+        
         return answer;
     }
 }
