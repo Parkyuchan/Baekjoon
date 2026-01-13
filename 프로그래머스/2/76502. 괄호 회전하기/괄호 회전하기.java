54afab3b36b7a64ce52910b42fe2c00e877868d0
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2026-01-14 00:11:33 +0900

    [level 2] Title: 괄호 회전하기, Time: 308.68 ms, Memory: 390 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,40 +2,47 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-
+        Deque<Character> deque = new ArrayDeque<>();
+        
         for (int i = 0; i < s.length(); i++) {
-            Stack<String> stack = new Stack<>();
-            List<String> list = Arrays.asList(s.split(""));
+            deque.add(s.charAt(i));
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            Stack<Character> stack = new Stack<>();
             boolean check = true;
-
-            for (int j = 0; j < s.length(); j++) {
-                if (list.get(j).equals("{") || list.get(j).equals("(") || list.get(j).equals("[")) {
-                    stack.push(list.get(j));
-                } else {
+            String str = "";
+            for (int j = 0; j < deque.size(); j++) {
+                str += deque.getFirst();
+                deque.add(deque.pop());
+            }
+            
+            for (int j = 0; j < str.length(); j++) {
+                char now = str.charAt(j);
+                
+                if (now == ')' || now == '}' || now == ']') {
                     if (stack.isEmpty()) {
                         check = false;
                         break;
                     }
-                    else if (list.get(j).equals("}") && stack.peek().equals("{")) {
-                        stack.pop();
-                    }
-                    else if (list.get(j).equals("]") && stack.peek().equals("[")) {
-                        stack.pop();
-                    }
-                    else if (list.get(j).equals(")") && stack.peek().equals("(")) {
+                    
+                    char top = stack.peek();
+                    if ((now == ')' && top == '(') || (now == '}' && top == '{') || (now == ']' && top == '[')) {
                         stack.pop();
                     } else {
                         check = false;
                         break;
                     }
+                } else {
+                    stack.push(now);
                 }
             }
-
+                
             if (check && stack.isEmpty())
                 answer++;
-
-            String change = String.valueOf(s.charAt(0));
-            s = s.substring(1) + change;
+            
+            deque.add(deque.pop());
+            
         }
         
         return answer;
