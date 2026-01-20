92f62bafd28b8823c76126d05dda6fa22935219e
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2026-01-21 07:35:48 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,22 +3,21 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 0;
-        Map<String, Integer> map = new LinkedHashMap<>();
-
+        Map<String, Integer> map = new HashMap<>();
+        
         for (int i = 0; i < clothes.length; i++) {
             map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
         }
-
+        
         List<String> listKey = new ArrayList<>(map.keySet());
         List<Integer> listValue = new ArrayList<>(map.values());
-
+        
         if (listKey.size() > 1) {
-            int total = 1;
-            for (int i = 0; i < listValue.size(); i++) {
-                total *= (listValue.get(i) + 1);
-            }
-
-            answer += total - 1;
+            answer = 1;
+            for (int i = 0; i < listValue.size(); i++) 
+                answer *= (listValue.get(i) + 1);
+            
+            answer--;
         } else {
             answer = listValue.get(0);
         }
