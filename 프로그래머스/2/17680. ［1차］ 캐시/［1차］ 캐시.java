8899cd9cb16ad2cc12935fe37a592f86f9765aef
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2026-01-24 23:08:10 +0900

    [level 2] Title: [1차] 캐시, Time: 38.18 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -4,30 +4,23 @@ class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
         List<String> list = new LinkedList<>();
-
-        if (cacheSize == 0) {
-            answer += 5*cities.length;
-            return answer;
-        }
-
-        for (int i = 0; i<cities.length; i++) {
-            String city = cities[i].toLowerCase();
-            
-            if (!list.contains(city)) {
-                answer += 5;
-                if (list.size() == cacheSize) {
+        
+        if (cacheSize == 0)
+            return cities.length * 5;
+        
+        for (int i = 0; i < cities.length; i++) {
+            cities[i] = cities[i].toUpperCase();
+            if (list.contains(cities[i])) {
+                answer+=1;
+                list.remove(cities[i]);
+                list.add(cities[i]);
+            } else {
+                if (list.size() == cacheSize) 
                     list.remove(0);
-                    list.add(city);
-                } else {
-                    list.add(city);
-                }
-
-                continue;
+                answer += 5;
+                list.add(cities[i]);
             }
-
-            answer += 1;
-            list.remove(city);
-            list.add(city);
+            
         }
         
         return answer;
