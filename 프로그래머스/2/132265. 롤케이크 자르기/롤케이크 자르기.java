586cecc0f37c78553616565090c50bffa3c82a7b
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2026-01-26 14:57:51 +0900

    [level 2] Title: 롤케이크 자르기, Time: 359.59 ms, Memory: 174 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -10,16 +10,16 @@ class Solution {
             map2.put(topping[i], map2.getOrDefault(topping[i], 0) + 1);
         
         for (int i = 0; i < topping.length; i++) {
-            for (int j = i; j < i + 1; j++) {
-                map1.put(topping[j], map1.getOrDefault(topping[i], 0) + 1);
-                
-                if (map2.containsKey(topping[j])) {
-                    if (map2.get(topping[j]) == 1)
-                    map2.remove(topping[j]);
-                else if (map2.get(topping[j]) > 1)
-                    map2.put(topping[j], map2.get(topping[j]) - 1);
-                }
+            
+            map1.put(topping[i], map1.getOrDefault(topping[i], 0) + 1);
+
+            if (map2.containsKey(topping[i])) {
+                if (map2.get(topping[i]) == 1)
+                map2.remove(topping[i]);
+            else if (map2.get(topping[i]) > 1)
+                map2.put(topping[i], map2.get(topping[i]) - 1);
             }
+            
             if (map1.size() == map2.size())
                 answer++;
         }
