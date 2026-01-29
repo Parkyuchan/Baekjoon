bf1f873d00c7930d2d038a1a0d9ca71c4da6fbcd
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2026-01-29 20:07:06 +0900

    [level 2] Title: 튜플, Time: 66.18 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,26 +2,25 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        int[] answer;
         s = s.replaceAll("[{}]", "");
-
-        String[] str = s.split(",");
-        Map<Integer, Integer> map = new LinkedHashMap<>();
-
-
-        for (int i = 0; i<str.length; i++) {
-            int num = Integer.parseInt(str[i]);
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        String[] list = s.split(",");
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < list.length; i++) {
+            int now = Integer.parseInt(list[i]);
+            map.put(now, map.getOrDefault(now, 0) + 1);
         }
-
-        List<Integer> listValue = new LinkedList<>(map.values());
-        List<Integer> listKey = new LinkedList<>(map.keySet());
-        Collections.sort(listValue);
-
-        answer = new int[listKey.size()];
-        for (int i = 0; i<listKey.size(); i++) {
-            int idxList = listValue.indexOf(map.get(listKey.get(i)));
-            answer[answer.length - idxList - 1] = listKey.get(i);
+        
+        List<Integer> listKey = new ArrayList<>(map.keySet());
+        int[] answer = new int[listKey.size()];
+        
+        for (int i = listKey.size(); i > 0; i--) {
+            for (int j = 0; j < listKey.size(); j++) {
+                if (map.get(listKey.get(j)) == i) {
+                    answer[listKey.size() - i] = listKey.get(j);
+                    break;
+                }
+            }
         }
         
         return answer;
