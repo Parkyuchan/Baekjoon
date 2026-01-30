c3ad897094a4c96795924a21242804e8f4d20315
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2026-01-30 14:00:33 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.85 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,47 +1,43 @@
 import java.util.*;
 
 class Solution {
-    static int row, col;
-    static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    private static int[] dx = {-1, 1, 0, 0};
+    private static int[] dy = {0, 0, -1, 1};
+    private static boolean[][] visited;
+
     public int solution(int[][] maps) {
         int answer = 0;
-        row = maps.length;
-        col = maps[0].length;
-        visited = new boolean[row][col];
-
-        answer = bfs(0, 0, row - 1, col - 1, maps);
-        System.out.println(answer);
+        visited = new boolean[maps.length][maps[0].length];
+        answer = game(maps);
         return answer;
     }
     
-    public static int bfs(int startX, int startY, int endX, int endY, int[][] maps) {
-
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[] {startX, startY, 0});
-        visited[startX][startY] = true;
-
-        while(!queue.isEmpty()) {
-            int[] poll = queue.poll();
-
-            if (poll[0] == endX && poll[1] == endY) {
-                return poll[2] + 1;
-            }
-
-            for (int delta = 0; delta < 4; delta++) {
-                int nx = poll[0] + dx[delta];
-                int ny = poll[1] + dy[delta];
-
-                if (nx >= 0 && nx < row && ny >= 0 && ny < col && !visited[nx][ny] && maps[nx][ny] == 1) {
-                    queue.offer(new int[] {nx, ny, poll[2] + 1});
-
-                    visited[nx][ny] = true;
+    private int game(int[][] maps) { 
+        
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0, 0, 1});
+        visited[0][0] = true;
+        
+        while(!q.isEmpty()) {
+            int[] now = q.poll();
+            if (now[0] == maps[0].length - 1 && now[1] == maps.length - 1)
+                return now[2];
+        
+            for (int i = 0; i < 4; i++) {
+                int x = now[0] + dx[i];
+                int y = now[1] + dy[i];
+                if (x > -1 && x < maps[0].length && y > -1 && y < maps.length) {
+                    if (maps[y][x] != 0 && !visited[y][x]) {
+                        q.offer(new int[]{x, y, now[2] + 1});
+                        visited[y][x] = true;
+                    }
                 }
             }
+            
+            
         }
-
+        
         return -1;
-
+        
     }
 }
