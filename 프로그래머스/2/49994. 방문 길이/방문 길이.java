28f36023aeccf8d4e7ba9ef3fe1759aadb5a8dbe
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2026-02-01 22:40:38 +0900

    [level 2] Title: 방문 길이, Time: 19.09 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -3,50 +3,56 @@ import java.util.*;
 class Solution {
     public int solution(String dirs) {
         int answer = 0;
-        Set<String> visited = new HashSet<>();
-        int x = 0;
-        int y = 0;
-        int nx = 0;
-        int ny = 0;
-
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-
-        for (int i = 0; i<dirs.length(); i++) {
-            switch (dirs.charAt(i)) {
+        
+        Set<String> set = new HashSet<>();
+        int[] loc = {0, 0};
+        
+        for (int i = 0; i < dirs.length(); i++) {
+            char now = dirs.charAt(i);
+            String str = "";
+            switch(now) {
                 case 'U' :
-                    if (ny + dy[2] > 5)
-                        continue;
-                    ny += dy[2];
+                    if (checkMove(loc[1] + 1)) {
+                        str = "(" + loc[0] + ", " + loc[1] + ") -> (" + loc[0] + ", " + (loc[1] + 1) + ")";
+                        loc[1]++;
+                        set.add(str);
+                    }
                     break;
+                    
                 case 'D' :
-                    if (ny + dy[3] < -5)
-                        continue;
-                    ny += dy[3];
+                    if (checkMove(loc[1] - 1)) {
+                        str = "(" + loc[0] + ", " + (loc[1] - 1) + ") -> (" + loc[0] + ", " + loc[1] + ")";
+                        loc[1]--;
+                        set.add(str);
+                    }
                     break;
-                case 'L' :
-                    if (nx + dx[1] < -5)
-                        continue;
-                    nx += dx[1];
-                    break;
+                    
                 case 'R' :
-                    if (nx + dx[0] > 5)
-                        continue;
-                    nx += dx[0];
+                    if (checkMove(loc[0] + 1)) {
+                        str = "(" + loc[0] + ", " + loc[1] + ") -> (" + (loc[0] + 1) + ", " + loc[1] + ")";
+                        loc[0]++;
+                        set.add(str);
+                    }
+                    break;
+                    
+                case 'L' :
+                    if (checkMove(loc[0] - 1)) {
+                        str = "(" + (loc[0] - 1) + ", " + loc[1] + ") -> (" + loc[0] + ", " + loc[1] + ")";
+                        loc[0]--;
+                        set.add(str);
+                    }
                     break;
             }
-
-            String edgeFirst = x + "," + y + "->" + nx + "," + ny;
-            String edgeSecond = nx + "," + ny + "->" + x + "," + y;
-            if (!visited.contains(edgeFirst) && !visited.contains(edgeSecond)) {
-                visited.add(edgeFirst);
-                visited.add(edgeSecond);
-                answer++;
-            }
-
-            x = nx;
-            y = ny;
+            
         }
-        return answer;
+        
+        return set.size();
+    }
+    
+    private boolean checkMove(int value) {
+        if (value > 5 || value < -5)
+            return false;
+        
+        return true;
     }
 }
