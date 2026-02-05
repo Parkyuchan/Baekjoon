4b09eed0e97f1ebfd58173c9debb236e09739e1b
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2026-02-05 20:29:36 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 10.38 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,39 +1,27 @@
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        String change = "";
-
-        if (k == 10)
-            change = Integer.toString(n);
-        else
-            change = Integer.toString(n, k);
-
-        String[] split = change.split("0+");
-
-        for (int i = 0; i < split.length; i++) {
-            boolean check = isPrime(Long.parseLong(split[i]));
-
-            if (check)
+        String change = Integer.toString(n, k);
+        String[] list = change.split("0+");
+        
+        for (int i = 0; i < list.length; i++)
+            if (isPrime(Long.parseLong(list[i])))
                 answer++;
-        }
-
+        
         return answer;
     }
-
-    private static boolean isPrime(long num) {
-
-        if (num == 2)
-            return true;
-
-        if (num < 2 || num % 2 == 0)
+    
+    private boolean isPrime(long n) {
+        if (n < 2)
             return false;
-
-        for (long i = 3; i*i <= num; i+=2) {
-            if (num % i == 0)
+        
+        if (n == 2)
+            return true;
+        
+        for (long i = 3; i*i <= n; i+=2)
+            if (n % i == 0)
                 return false;
-        }
-
+        
         return true;
     }
-    
 }
